a40c01b673ff5888d3af88797b26f25a97f1fde2
tejas git <d9489079@gmail.com>
2025-12-28 16:51:26 +0530

    clerk as been added

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Chat App Server is running" });
+  res.json({ message: "Chat App Server is running at WSA" });
 });
 
 export { app };
