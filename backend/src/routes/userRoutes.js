0fab4abe8710a77348a1db3df53c257240fb02cc
tejas git <d9489079@gmail.com>
2026-01-04 11:05:29 +0530

    user authentication tessting is done in backend using postman

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { handleClerkWebhook } from "../../controllers/userController.js";
+import {
+  getUsersForSidebar,
+  handleClerkWebhook,
+} from "../../controllers/userController.js";
+import { requireAuth } from "@clerk/express";
 
 const userRouter = express.Router();
 
@@ -8,4 +12,5 @@ userRouter
   .route("/webhooks")
   .post(bodyParser.raw({ type: "application/json" }), handleClerkWebhook);
 
+userRouter.route("/getall").get(requireAuth(), getUsersForSidebar);
 export default userRouter;
